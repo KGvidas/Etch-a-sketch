c8c2310e50db35f8ce6d7ed073f41614d9fc5b35
KG <122453305+KGvidas@users.noreply.github.com>
2023-08-19 21:32:31 +0300

    Add create and reset grid functions

--- javascript.js
+++ javascript.js
@@ -6,6 +6,32 @@ let input = document.getElementById('myRange');
 
 input.addEventListener("input", (event) => {
     value.textContent = event.target.value + " x " + event.target.value;
-    console.log(value);
+
+    resetGrid()
+
+    let oneNumberValue = event.target.value;
+    // console.log(oneNumberValue);
+    createGrid(oneNumberValue);
 });
 // RANGE SLIDER  END
+ 
+let containerGrid = document.querySelector('.containerGrid')
+
+function createGrid(oneNumberValue) {
+    for (let i = 0; i < oneNumberValue; i++){
+        let div = document.createElement('div');
+        div.classList.add('addedDiv');
+        div.textContent = "hello";
+        containerGrid.appendChild(div);
+    }
+};
+
+function resetGrid(){
+    const removeDivs = document.querySelectorAll('.addedDiv');
+     removeDivs.forEach(div => div.remove());
+};
+
+
+
+// toggle shading
+// toggle lightnen
